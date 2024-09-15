3de2931a625d4a0f12f702904773dc6ff879fa36
Pavan Hegde <pavanrkadave@gmail.com>
2024-09-15 23:38:01 +0200

    Update api-gateway

---
+++ src/test/java/xyz/pavanhegde/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pavanhegde.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
